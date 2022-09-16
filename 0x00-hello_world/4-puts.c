ba7e04d98d8ea442389e2041bceaf0fffd01cec2
kkeas <keaskatrina@gmail.com>
2022-09-15 18:42:21 -0700

    betty errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
+/*prints text and returns 0*/
 
 int main(void)
 {
-    
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
-  
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
