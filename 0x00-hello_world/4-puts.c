b8498d80eade5814ded8a1051720ef2071ede491
kkeas <keaskatrina@gmail.com>
2022-09-15 18:18:02 -0700

    question 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
-#!/bin/bash
-int main (void)
-{
-  puts("Programming is like building a multilingual puzzle"\n)
+#include <stdio.h>
 
-    return (0)
+int main(void)
+{
+    
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+  
 }
