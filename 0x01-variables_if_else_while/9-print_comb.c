72a5d7080a75677a549c6b938f852b6a58736278
kkeas <keaskatrina@gmail.com>
2022-09-20 13:40:30 -0700

    question 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * return: always 0
+ */
+
+int main(void)
+{
+int n;
+
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+
+if (n != '9')
+
+	  putchar(',');
+	  putchar(' ');
+}
+putchar('\n');
+return (0);
+}
