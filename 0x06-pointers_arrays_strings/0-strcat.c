2f3427e8fb5fb3e7a3cbd4d665c8d2f70415a0fa
kkeas <keaskatrina@gmail.com>
2022-09-29 15:18:25 -0700

    question 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_strcat - function to concatenate two strings
+ *@dest: the string to concatenate to
+ *@src: the string to add
+ *Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int x, y;
+
+	for (x = 0; dest[x] != '\0'; x++)
+		;
+	for (y = 0; src[y] != '\0'; y++)
+	{
+		dest[x] = src[y];
+		y++;
+	}
+	dest[x] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a readme for project 6 about pointers, arrays, and strings.
