bf63d6e10f14cfc1565f725faf135941b42c1bd7
kkeas <keaskatrina@gmail.com>
2022-11-03 12:00:48 -0700

    add a node to the beginning of the list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ *add_node - add a node at the beginning of the list
+ *@head: the beginnning of the list
+ *@str: the string to duplicate
+ *Return: address of the new element of NULL if failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	int count, i = 0;
+
+	if (head == NULL)
+		return (NULL);
+	
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+
+	new_node->len = i;
+	new_node->str = strdup(str);
+	new_node->next = *head;
+
+	*head = new_node;
+
+	return (new_node);
+}
